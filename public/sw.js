487ef2e44b135dc6b4f66865473036c81c7d092d
Walter Wills <walter@example.com>
2026-03-04 12:34:18 -0600

    Iron Halo Verify v4.0 - fresh Dynamsoft v11 build

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-// Self-destruct: unregister this service worker and stop intercepting requests
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(self.registration.unregister());
-});
